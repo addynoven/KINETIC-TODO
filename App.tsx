908721da31773a173add6c47d1c8c567d1fbe018
addynoven <dmcbaditya@gmail.com>
2026-08-19 21:15:06 +0530

    task 1 done

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // task 1 done
     <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
       <View style={{backgroundColor: 'red', width: 100, height: 100, alignSelf: 'flex-start'}}>
       </View>
