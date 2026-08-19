684daba686959be1984f5d82b8152ce1a841cd65
addynoven <dmcbaditya@gmail.com>
2026-08-19 21:14:20 +0530

    Refactor App component layout and remove unused styles

--- App.tsx
+++ App.tsx
@@ -3,18 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+      <View style={{backgroundColor: 'red', width: 100, height: 100, alignSelf: 'flex-start'}}>
+      </View>
+      <View style={{backgroundColor: 'blue', width: 100, height: 100,alignSelf: 'center'}}>
+      </View>
+      <View style={{backgroundColor: 'green', width: 100, height: 100, alignSelf: 'flex-end'}}>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
