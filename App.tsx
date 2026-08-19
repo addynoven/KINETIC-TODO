325953c5dcf07d7f1167f812fa9710636ade6f6a
addynoven <dmcbaditya@gmail.com>
2026-08-19 21:38:24 +0530

    task 2 done

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {  Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // task 2 color change
+  const carr =["#090D16", "#1E1B4B", "#064E3B", "#701A75", "#7C2D12"]
+  const [index, setIndex] = useState(0);
+  const handlePress = () => {
+    setIndex((index + 1) % carr.length);
+  }
   return (
-    // task 1 done
-    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <View style={{backgroundColor: 'red', width: 100, height: 100, alignSelf: 'flex-start'}}>
-      </View>
-      <View style={{backgroundColor: 'blue', width: 100, height: 100,alignSelf: 'center'}}>
-      </View>
-      <View style={{backgroundColor: 'green', width: 100, height: 100, alignSelf: 'flex-end'}}>
-      </View>
-      <StatusBar style="auto" />
+    <View style={{flex:1, backgroundColor: carr[index], alignItems: "center", justifyContent: "center"}}>
+        <Pressable onPress={handlePress}>
+        <View style={{width: 100, height: 100, backgroundColor: "#fff", alignItems: "center", justifyContent: "center"}}>
+          <Text style={{color: "#000", fontSize: 32}}>{carr[index]}</Text>
+          <Text style={{color: "#000"}}>Press Me</Text>
+        </View>
+     </Pressable>
     </View>
   );
 }
