ce841d178378fec228065f3b19a3539fe2e45f4b
addynoven <dmcbaditya@gmail.com>
2026-08-19 21:59:39 +0530

    task 3 done

--- App.tsx
+++ App.tsx
@@ -1,22 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import {  Pressable, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  // task 2 color change
-  const carr =["#090D16", "#1E1B4B", "#064E3B", "#701A75", "#7C2D12"]
-  const [index, setIndex] = useState(0);
-  const handlePress = () => {
-    setIndex((index + 1) % carr.length);
-  }
+  //task 3 view card
   return (
-    <View style={{flex:1, backgroundColor: carr[index], alignItems: "center", justifyContent: "center"}}>
-        <Pressable onPress={handlePress}>
-        <View style={{width: 100, height: 100, backgroundColor: "#fff", alignItems: "center", justifyContent: "center"}}>
-          <Text style={{color: "#000", fontSize: 32}}>{carr[index]}</Text>
-          <Text style={{color: "#000"}}>Press Me</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#090D16",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "white",
+          borderRadius: 20,
+          alignItems: "center",
+          justifyContent: "space-around",
+          flexDirection: "row",
+          gap: 20,
+          padding: 20,
+        }}
+      >
+        <View style={{ width: 100, height: 100, borderRadius: 20 }}>
+          <Image
+            source={{ uri: "https://picsum.photos/id/237/200/300" }}
+            style={{ width: 100, height: 100, borderRadius: 20 }}
+          />
         </View>
-     </Pressable>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>neon (@neon)</Text>
+          <Text style={{ fontSize: 16, color: "gray" }}>
+            Mobile & AI Dev 🚀
+          </Text>
+        </View>
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
