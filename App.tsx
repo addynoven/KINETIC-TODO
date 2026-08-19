e06559792dca4666dbf217ddb4d0180d85eb4c9c
addynoven <dmcbaditya@gmail.com>
2026-08-19 22:18:03 +0530

    task 4 done "Implement Live Input Mirror and Dynamic Preview in App component"

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-  //task 3 view card
+  // task 4 Live Input Mirror & Dynamic Preview (Controlled Inputs)
+  const [text, setText] = useState("");
   return (
     <View
       style={{
@@ -13,31 +21,63 @@ export default function App() {
         justifyContent: "center",
       }}
     >
+      <TextInput
+        placeholder="Enter some text..."
+        value={text}
+        onChangeText={setText}
+        style={{
+          fontSize: 16,
+          color: "white",
+          borderWidth: 1,
+          borderColor: "gray",
+          padding: 10,
+          backgroundColor: "#1E1E1E",
+          borderRadius: 5,
+          width: "80%",
+        }}
+      />
       <View
         style={{
-          backgroundColor: "white",
-          borderRadius: 20,
+          marginTop: 20,
           alignItems: "center",
-          justifyContent: "space-around",
-          flexDirection: "row",
-          gap: 20,
-          padding: 20,
+          width: "100%",
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          backgroundColor: "#fefefe",
+          borderRadius: 10,
         }}
       >
-        <View style={{ width: 100, height: 100, borderRadius: 20 }}>
-          <Image
-            source={{ uri: "https://picsum.photos/id/237/200/300" }}
-            style={{ width: 100, height: 100, borderRadius: 20 }}
-          />
-        </View>
-        <View>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>neon (@neon)</Text>
-          <Text style={{ fontSize: 16, color: "gray" }}>
-            Mobile & AI Dev 🚀
-          </Text>
-        </View>
+        <Text style={{ fontSize: 16, alignSelf: "flex-start" }}>
+          PREVIEW CARD
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+          }}
+        >
+          {text}
+        </Text>
+        <Text>
+          {text.length ? `Total characters: ${text.length}` : "No text entered"}
+        </Text>
       </View>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          marginTop: 20,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#fefefe",
+          padding: 10,
+          borderRadius: 10,
+        }}
+      >
+        <Pressable>
+          <Text style={{ fontSize: 16 }} onPress={() => setText("")}>
+            Clear text
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
